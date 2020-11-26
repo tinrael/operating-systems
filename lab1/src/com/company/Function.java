6cbffdfb947c5a0b70ac097115605eee2dd9a262
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-11-27 01:35:42 +0200

    Add the Function class

---
+++ lab1/src/com/company/Function.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.nio.channels.Pipe;
+
+public abstract class Function implements Runnable {
+    Pipe.SinkChannel sinkChannel;
+
+    private final int x;
+
+    public Function(Pipe.SinkChannel sinkChannel, int x) {
+        this.sinkChannel = sinkChannel;
+        this.x = x;
+    }
+
+    public abstract void compute();
+
+    @Override
+    public void run() {
+        compute();
+    }
+}
