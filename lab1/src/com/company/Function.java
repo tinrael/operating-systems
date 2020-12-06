64abfb06a48b4167c59397d4e90c2a2f28c8f80f
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-12-07 01:07:23 +0200

    Update the Function class

--- lab1/src/com/company/Function.java
+++ lab1/src/com/company/Function.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.Pipe;
 
 public abstract class Function implements Runnable {
@@ -12,11 +14,25 @@ public abstract class Function implements Runnable {
         this.x = x;
     }
 
-    public abstract void compute();
+    public abstract int compute() throws InterruptedException;
+
+    private void write(int result) throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(4);
+        buffer.putInt(result);
+        buffer.flip();
+        while (buffer.hasRemaining()) {
+            sinkChannel.write(buffer);
+        }
+    }
 
     @Override
     public void run() {
-        compute();
+        try {
+            int result = compute();
+            write(result);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public int getX() {
