1e79e0d8a27165cc1a56edf4d90371eb06588be1
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-12-07 04:02:49 +0200

    Update the compute() method

--- lab1/src/com/company/FunctionF.java
+++ lab1/src/com/company/FunctionF.java
@@ -6,12 +6,36 @@ import java.nio.channels.Pipe;
 
 public class FunctionF extends Function {
 
-    public FunctionF(Pipe.SinkChannel sinkChannel, int x) {
-        super(sinkChannel, x);
+    public FunctionF(Pipe.SinkChannel sinkChannel, int x, boolean isTrialFunction) {
+        super(sinkChannel, x, isTrialFunction);
+
     }
 
     @Override
     public int compute() throws InterruptedException {
-        return IntOps.funcF(getX());
+        if (isTrialFunction()) {
+            return IntOps.funcF(getX());
+        }
+
+        switch(getX()) {
+            case 0:
+                Thread.sleep(3000);
+                return 9;
+            case 1:
+                Thread.sleep(6000);
+                return 6;
+            case 2:
+                Thread.sleep(2000);
+                return 0;
+            case 3:
+                while (true) {}
+            case 4:
+                Thread.sleep(2000);
+                return 7;
+            case 5:
+                while (true) {}
+            default:
+                return 0;
+        }
     }
 }
--- lab1/src/com/company/FunctionG.java
+++ lab1/src/com/company/FunctionG.java
@@ -6,12 +6,35 @@ import java.nio.channels.Pipe;
 
 public class FunctionG extends Function {
 
-    public FunctionG(Pipe.SinkChannel sinkChannel, int x) {
-        super(sinkChannel, x);
+    public FunctionG(Pipe.SinkChannel sinkChannel, int x, boolean isTrialFunction) {
+        super(sinkChannel, x, isTrialFunction);
     }
 
     @Override
     public int compute() throws InterruptedException {
-        return IntOps.funcG(getX());
+        if (isTrialFunction()) {
+            return IntOps.funcG(getX());
+        }
+
+        switch(getX()) {
+            case 0:
+                Thread.sleep(6000);
+                return 5;
+            case 1:
+                Thread.sleep(3000);
+                return 2;
+            case 2:
+                while (true) {}
+            case 3:
+                Thread.sleep(2000);
+                return 0;
+            case 4:
+                while (true) {}
+            case 5:
+                Thread.sleep(2000);
+                return 8;
+            default:
+                return 0;
+        }
     }
 }
