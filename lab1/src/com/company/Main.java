113977164fe8540524dd948a2fb5fa2306619e90
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-12-07 04:04:29 +0200

    Add example of usage

--- lab1/src/com/company/Main.java
+++ lab1/src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        Manager manager = new Manager(6, false);
+        manager.compute();
     }
 }
