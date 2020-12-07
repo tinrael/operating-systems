7b44f3f5c6eb0d29812942f9947688912635f1c3
Timur Krasnopir <timur.krasnopir@unicyb.kiev.ua>
2020-12-07 03:42:57 +0200

    Implement the compute() method

--- lab1/src/com/company/Manager.java
+++ lab1/src/com/company/Manager.java
@@ -11,7 +11,11 @@ public class Manager {
     private Pipe pipeG;
 
     public Manager(int x) throws IOException {
-        this.x = x;
+        if (x > 6 || x < 1) {
+            System.out.println("[Manager]: Incorrect value of 'x'.");
+            System.exit(0);
+        }
+        this.x = x - 1;
 
         pipeF = Pipe.open();
         pipeF.source().configureBlocking(false);
@@ -20,5 +24,72 @@ public class Manager {
         pipeG = Pipe.open();
         pipeG.source().configureBlocking(false);
         pipeG.sink().configureBlocking(false);
+    }
+
+    public void compute() throws IOException {
+        ByteBuffer bufferF = ByteBuffer.allocate(4);
+        ByteBuffer bufferG = ByteBuffer.allocate(4);
+
+        Pipe.SourceChannel sourceChannelF = pipeF.source();
+        Pipe.SourceChannel sourceChannelG = pipeG.source();
+
+        Thread keyConsoleListenerThread = new Thread(new KeyConsoleListener());
+        keyConsoleListenerThread.start();
+
+        (new Thread(new FunctionF(pipeF.sink(), x))).start();
+        (new Thread(new FunctionG(pipeG.sink(), x))).start();
+
+        int resultF = 0;
+        boolean isFComputed = false;
+        int resultG = 0;
+        boolean isGComputed = false;
+
+        while (keyConsoleListenerThread.isAlive()) {
+            if (bufferF.hasRemaining()) {
+                sourceChannelF.read(bufferF);
+                if (!bufferF.hasRemaining()) {
+                    bufferF.flip();
+                    resultF = bufferF.getInt();
+                    isFComputed = true;
+                    System.out.println("[Manager]: f(" + x + ") = " + resultF + ".");
+                    if (resultF == 0) {
+                        System.out.println("[Manager]: The final result can be computed immediately, because f(" + x + ") = 0.");
+                        System.out.println("[Manager]: f(" + x + ") * g(" + x + ") = 0.");
+                        System.exit(0);
+                    }
+                }
+            }
+
+            if (bufferG.hasRemaining()) {
+                sourceChannelG.read(bufferG);
+                if (!bufferG.hasRemaining()) {
+                    bufferG.flip();
+                    resultG = bufferG.getInt();
+                    isGComputed = true;
+                    System.out.println("[Manager]: g(" + x + ") = " + resultG + ".");
+                    if (resultG == 0) {
+                        System.out.println("[Manager]: The final result can be computed immediately, because g(" + x + ") = 0.");
+                        System.out.println("[Manager]: f(" + x + ") * g(" + x + ") = 0.");
+                        System.exit(0);
+                    }
+                }
+            }
+
+            if (!bufferF.hasRemaining() && !bufferG.hasRemaining()) {
+                System.out.println("[Manager]: f(" + x + ") * " + "g(" + x + ") = " + (resultF * resultG) + ".");
+                System.exit(0);
+            }
+        }
+
+        System.out.println("[Manager]: The computation was canceled by the user.");
+        System.out.print("[Manager]: The final result can not be computed as ");
+        if (!isFComputed && !isGComputed) {
+            System.out.println("f(" + x + ") and g(" + x + ") are not computed.");
+        } else if (!isFComputed) {
+            System.out.println("f(" + x + ") is not computed.");
+        } else if (!isGComputed){
+            System.out.println("g(" + x + ") is not computed.");
+        }
+        System.exit(0);
     }
 }
